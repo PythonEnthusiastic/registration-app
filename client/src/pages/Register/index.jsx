e1cde90ce9eaa84e7bfe59a26b65b5df3edcbc68
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2024-01-05 22:27:39 -0700

    Added basic registration and registration state

--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
+import useInput from "../../hooks/useInput.js";
+
 import Navbar from "../../components/Navbar/index.jsx"
 
 const Register = () => {
+    const [input, setInput] = useInput();
+
     return (
         <>
             <Navbar />
             <h1>Register Page</h1>
+            <form>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email" onChange={setInput} />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" onChange={setInput} />
+            </form>
         </>
     )
 }
