d0844fa28d63f3b391076e54133e9cc9f12b76b8
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2024-01-20 23:03:57 -0700

    Removed unecessary code

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ const PORT = 4000;
 app.use(express.json())
 app.use(cookieParser())
 
-app.get("/api/v1", (req, res) => {
-    res.send("hello")
-})
-
 app.post("/api/v1/register", (req, res) => {
     try {
         const credentials = JSON.parse(req.body.data);
