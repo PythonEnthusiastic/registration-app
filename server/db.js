f5eed03dce24b21ec506ca162d87a85502f91724
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2024-02-06 02:14:26 -0700

    added inserting data to database, still need to fix a few bugs

--- server/db.js
+++ server/db.js
@@ -1,20 +1,30 @@
+require('dotenv').config()
 const mongoose = require("mongoose");
-const uri = process.env.URI;
+console.log(process.env.URI)
+const dbUrl = process.env.URI;
 
-main().catch((err) => console.log(err))
+mongoose.connect(dbUrl);
 
-const main = async () => {
-    await mongoose.connect(uri);
+const udb = mongoose.connection;
+udb.on('error', console.error.bind(console, 'connection error: '));
 
-    const studentSchema = new mongoose.Schema({
-        username: String,
-        email: String,
-        hash: String,
-        isAdmin: Boolean,
-        firstName: String,
-        lastName: String,
-        telephone: String,
-        Address: String,
-        createDate: { type: Date, default: Date.now }
-    });
+const studentSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    hash: String,
+    isAdmin: Boolean,
+    firstName: String,
+    lastName: String,
+    telephone: String,
+    Address: String,
+    createDate: { type: Date, default: Date.now }
+});
+
+const student = mongoose.model("student", studentSchema)
+
+exports.createUser = (data) => {
+    const newUser = new student();
+
+    newUser.username = data.username
+    newUser.save()
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const tokenizer = require("./token.js")
 const hash = require("./hash.js")
+const db = require("./db.js");
 
 const app = express();
 const PORT = 4000;
@@ -14,6 +15,8 @@ app.post("/api/v1/register", (req, res) => {
         const credentials = JSON.parse(req.body.data);
         // const hashed = hash.encrypt(credentials.password)
 
+        db.createUser(credentials)
+
         res.sendStatus(200)
     } catch (err) {
         res.sendStatus(400)
@@ -21,12 +24,13 @@ app.post("/api/v1/register", (req, res) => {
 })
 
 app.post("/api/v1/login", (req, res) => {
+    console.log("hi")
     try {
         const credentials = JSON.parse(req.body.data);
-        const tokened = tokenizer.generate(credentials);
+        // const tokened = tokenizer.generate(credentials);
         // const hashed = hash.encrypt(credentials.password);
 
-        res.cookie('userID', JSON.stringify(tokened), { httpOnly: true })
+        // res.cookie('userID', JSON.stringify(tokened), { httpOnly: true })
         res.sendStatus(200)
     } catch (err) {
         res.sendStatus(400)
