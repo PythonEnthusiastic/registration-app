6f1c06c78bde2515366354640d5af01944e9a007
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2024-02-05 23:55:21 -0700

    Added schema

--- server/db.js
+++ server/db.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose");
+const uri = process.env.URI;
 
 main().catch((err) => console.log(err))
 
 const main = async () => {
-    await mongoose.connect('test')
+    await mongoose.connect(uri);
+
+    const studentSchema = new mongoose.Schema({
+        username: String,
+        email: String,
+        hash: String,
+        isAdmin: Boolean,
+        firstName: String,
+        lastName: String,
+        telephone: String,
+        Address: String,
+        createDate: { type: Date, default: Date.now }
+    });
 }
